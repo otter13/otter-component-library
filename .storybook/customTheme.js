3ec59dfcd07f1b6f72bb071d59901463312f5ffa
Jin Li <limosmiley@gmail.com>
2022-12-14 14:13:54 +1100

    Add storybook init

---
+++ .storybook/customTheme.js
@@ -0,0 +1,9 @@
+import { create } from '@storybook/theming';
+
+export default create({
+  base: 'light',
+  brandTitle: 'Otter UI Library',
+  brandUrl: '/.',
+  //brandImage: 'static/media/src/stories/assets/logo-icon.svg',
+  brandTarget: '_self',
+});
